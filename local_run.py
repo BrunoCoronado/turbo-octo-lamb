d8e8ff6dcf4528b02884aff8a07ae58ddfc26edf
bcoronado <bcoronado.morales@gmail.com>
2026-09-03 19:05:49 -0600

    feat(sales): add service to fetch mock deposits data

--- local_run.py
+++ local_run.py
@@ -9,6 +9,7 @@ sys.path.append(str(SRC_PATH))
 # Importar las funciones Lambda
 from client_data.inventory.inventory_handler import lambda_handler as inventory_handler
 from client_data.purchases.purchases_handler import lambda_handler as purchases_handler
+from client_data.sales.sales_handler import lambda_handler as sales_handler
 
 if __name__ == "__main__":
     # Evento de API Gateway simulado (vaciado por ahora ya que solo es un GET simple)
@@ -30,5 +31,12 @@ if __name__ == "__main__":
         
     # Invocar directamente el handler (context puede ir como None o un objeto dummy)
     response_inventory = purchases_handler(fake_event, None)
+    print("Status Code:", response_inventory["statusCode"])
+    print("Body Output:", json.loads(response_inventory["body"])) # Muestra el primer registro
+    
+    print("--- 3. Probando GET /client-data/sales ---")
+        
+    # Invocar directamente el handler (context puede ir como None o un objeto dummy)
+    response_inventory = sales_handler(fake_event, None)
     print("Status Code:", response_inventory["statusCode"])
     print("Body Output:", json.loads(response_inventory["body"])) # Muestra el primer registro
