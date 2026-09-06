c77cb7c7846672349f72fd2c5d307a39c78027fd
bcoronado <bcoronado.morales@gmail.com>
2026-09-06 15:45:57 -0600

    chore(s3-athena): Add SQL script for Athena database definitions

---
+++ athena-scripts.sql
@@ -0,0 +1,57 @@
+--Database creation
+CREATE DATABASE turbo_octo_lamb
+
+--Table definition for intentory data
+CREATE EXTERNAL TABLE IF NOT EXISTS turbo_octo_lamb.inventory(
+    inventoryItemCode BIGINT,
+    inventoryItemId INT,
+    inventoryItemDescription STRING,
+    initialInventory DECIMAL(11,4),
+    mov DECIMAL(11,4),
+    value DECIMAL(11,4)
+)
+--How to parse and interpret each line on csv file
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+   'separatorChar' = ',',
+   'quoteChar'     = '"',
+   'escapeChar'    = '\\'
+)
+LOCATION 's3://turbo-octo-lamb-mock-data-113008552223-us-east-1-an/inventory/'
+TBLPROPERTIES ('skip.header.line.count'='1');
+
+--Table definition for purchases data
+CREATE EXTERNAL TABLE IF NOT EXISTS turbo_octo_lamb.purchases(
+    itemPurchaseId INT,
+    businessDate STRING,
+    itemPurchaseReferenceNumber INT,
+    itemPurchaseComment STRING
+)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+   'separatorChar' = ',',
+   'quoteChar'     = '"',
+   'escapeChar'    = '\\'
+)
+LOCATION 's3://turbo-octo-lamb-mock-data-113008552223-us-east-1-an/purchases/'
+TBLPROPERTIES ('skip.header.line.count'='1');
+
+ALTER TABLE turbo_octo_lamb.purchases 
+SET TBLPROPERTIES ('use.null.for.invalid.data' = 'true');
+
+--Table definition for sales data
+CREATE EXTERNAL TABLE IF NOT EXISTS turbo_octo_lamb.sales(
+    depositId INT,
+    depositTime STRING,
+    businessDate STRING,
+    salesAreaName STRING,
+    voucherNumber STRING
+)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+   'separatorChar' = ',',
+   'quoteChar'     = '"',
+   'escapeChar'    = '\\'
+)
+LOCATION 's3://turbo-octo-lamb-mock-data-113008552223-us-east-1-an/sales/'
+TBLPROPERTIES ('skip.header.line.count'='1');
